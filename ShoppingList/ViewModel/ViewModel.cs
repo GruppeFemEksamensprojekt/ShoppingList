279fb522f5e15e242aaa8cb79a82e3865b3affc6
Lausius <skinkens_email@yahoo.dk>
2020-02-10 10:10:28 +0100

    selectedshopping list

--- ShoppingList/ViewModel/ViewModel.cs
+++ ShoppingList/ViewModel/ViewModel.cs
@@ -10,6 +10,30 @@ namespace ShoppingList.ViewModel
 {
     class ViewModel
     {
+        #region Instance Fields
+        private ShoppingListCatalog _selectedShoppingList;
+        #endregion
+
+        #region Constructor
+        public ViewModel()
+        {
+
+        }
+        #endregion
+
+        #region Properties
         public ObservableCollection<ProductModel> ProductList { get; set; }
+        public ShoppingListCatalog SelectedShoppingList
+        {
+            get { return _selectedShoppingList; }
+            set { _selectedShoppingList = value; }
+        }
+        #endregion
+        #region Methods
+        public bool ShoppingListIsSelected()
+        {
+            return SelectedShoppingList != null;
+        }
+        #endregion
     }
 }
