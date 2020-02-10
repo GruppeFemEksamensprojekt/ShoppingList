d991a3df4da77cee73f5db1407e880d242ed10cf
Kasper Stilling Andersen <kasp762s@edu.easj.dk>
2020-02-10 10:30:00 +0100

    Show

--- ShoppingList/Model/ShoppingListModel.cs
+++ ShoppingList/Model/ShoppingListModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,30 @@ namespace ShoppingList.Model
     public class ShoppingListModel
     {
         //Indkøbsliste Model
+        public string _shoppinglistName;
+        public ObservableCollection<ProductModel> _productCatalog;
 
+        public ShoppingListModel(string shoppinglistName)
+        {
+            _productCatalog = new ObservableCollection<ProductModel>();
+
+        }
+
+        public ObservableCollection<ProductModel> ProductCatalog
+        {
+            get 
+            {
+                return _productCatalog; 
+            } 
+            set
+            {
+                _productCatalog = value; 
+            } 
+        }
+        public string ListIsEmpty
+        {
+            get;
+            set;
+        }
     }
 }
