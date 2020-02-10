f275bde676992f0afb6f75536a53dd063bf2554f
HappyHeirloom <jonasgross@hotmail.com>
2020-02-10 13:46:18 +0100

    Added amoutntype
    Added amounttype to productmodel.cs

--- ShoppingList/Model/ProductModel.cs
+++ ShoppingList/Model/ProductModel.cs
@@ -12,19 +12,22 @@ namespace ShoppingList.Model
         private string _itemName;
         private string _store;
         private int _itemAmount;
+        private string _itemAmountType;
         private double _itemPrice;
 
-        public ProductModel(string itemName, string store, int itemAmount, double itemPrice)
+        public ProductModel(string itemName, string store, int itemAmount, string itemAmountType, double itemPrice)
         {
             _itemName = itemName;
             _store = store;
             _itemAmount = itemAmount;
+            _itemAmountType = itemAmountType;
             _itemPrice = itemPrice;
         }
 
         public string ItemName { get; set; }
         public string Store { get; set; }
         public int ItemAmount { get; set; }
+        public string ItemAmountType { get; set; }
         public double ItemPrice { get; set; }
     }
 }
