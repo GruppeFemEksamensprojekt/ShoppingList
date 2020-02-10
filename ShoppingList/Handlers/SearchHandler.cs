42da3ce6db9ac64ef2f76df982bb62229381ff21
Jakob-Droid <58071936+Jakob-Droid@users.noreply.github.com>
2020-02-10 12:26:48 +0100

    SearchEngine

--- ShoppingList/Handlers/SearchHandler.cs
+++ ShoppingList/Handlers/SearchHandler.cs
@@ -19,16 +19,53 @@ namespace ShoppingList.Handlers
             productCatalog = new ObservableCollection<ProductModel>();
             Bought = false;
         }
-        public ObservableCollection<ProductModel> SearchList1(string search)
+
+        public ObservableCollection<ProductModel> SearchListName(string search)
         {
             try
             {
-                var list = productCatalog.Where(product => product.ItemName.Contains(search.ToLower()));
+                var list = productCatalog.Where(product => product.ItemName.Contains(search.ToLower().ToLower()));
                 return new ObservableCollection<ProductModel>(list);
             }
             catch (Exception e)
             {
-                Console.WriteLine($"Din søgning på {search}, gav ingen resultater\n{e}");
+                Console.WriteLine($"Din søgning på produktet {search}, gav ingen resultater\n{e}");
+            }
+
+            return null;
+        }
+
+        public ObservableCollection<ProductModel> SearchListStore(string search)
+        {
+            try
+            {
+                var list = productCatalog.Where(product => product.Store.ToLower().Contains(search.ToLower()));
+                return new ObservableCollection<ProductModel>(list);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Din søgning på butikken {search}, gav ingen resultater\n{e}");
+            }
+
+            return null;
+        }
+
+        public ObservableCollection<ProductModel> SearchEngine(string search)
+        {
+            try
+            {
+                if (SearchListName(search) == null)
+                {
+                    SearchListStore(search);
+                }
+                else if (SearchListStore(search) == null)
+                {
+                    SearchListName(search);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Din søgning på butikken {search}, gav ingen resultater\n{e}");
             }
 
             return null;
