f8295b0c2138e789d95c2a8fa5a5332de41a24ea
Kasper Stilling Andersen <kasp762s@edu.easj.dk>
2020-02-10 09:35:34 +0100

    hello world

---
+++ ShoppingList/Model/ShoppingListCatalog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Model
+{
+    public class ShoppingListCatalog
+    {
+        //Liste over indkøbslister
+    }
+}
