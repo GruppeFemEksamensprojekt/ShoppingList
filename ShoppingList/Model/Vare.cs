88d660d61a5c935e6baa37d9eda710307c1b4b35
HappyHeirloom <jonasgross@hotmail.com>
2020-02-10 13:40:38 +0100

    CreateItem
    Needs designing

---
+++ ShoppingList/Model/Vare.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Model
+{
+    class Vare
+    {
+
+        private string _name;
+        private int _quantity;
+        private string _quantityType;
+        private int _price;
+        private string _shop;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+            }
+        }
+
+
+
+    }
+}
