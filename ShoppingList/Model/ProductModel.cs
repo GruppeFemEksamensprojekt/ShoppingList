ac36988e155fbdd32628ef8f7bad0d7eecac018f
Kasper Stilling Andersen <kasp762s@edu.easj.dk>
2020-02-10 10:10:26 +0100

    i

--- ShoppingList/Model/ProductModel.cs
+++ ShoppingList/Model/ProductModel.cs
@@ -9,5 +9,22 @@ namespace ShoppingList.Model
     public class ProductModel
     {
         // Vare model
+        public string _itemName;
+        public string _store;
+        public int _itemAmount;
+        public double _itemPrice;
+
+        public ProductModel(string itemName, string store, int itemAmount, double itemPrice)
+        {
+            _itemName = itemName;
+            _store = store;
+            _itemAmount = itemAmount;
+            _itemPrice = itemPrice;
+        }
+
+        public string ItemName { get; set; }
+        public string Store { get; set; }
+        public int ItemAmount { get; set; }
+        public double ItemPrice { get; set; }
     }
 }
