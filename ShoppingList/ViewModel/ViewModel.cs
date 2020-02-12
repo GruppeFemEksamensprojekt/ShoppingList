4427a2eedc851ccaaee77d61ef935c5c15ed59ad
Kasper Stilling Andersen <kasp762s@edu.easj.dk>
2020-02-12 15:17:42 +0100

    hmm

--- ShoppingList/ViewModel/ViewModel.cs
+++ ShoppingList/ViewModel/ViewModel.cs
@@ -12,10 +12,12 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using ShoppingList.Persistancy;
 using Windows.UI.Popups;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace ShoppingList.ViewModel
 {
-    class ViewModel
+    public class ViewModel : INotifyPropertyChanged
     {
         #region Instance Fields
         private ShoppingListModel _selectedShoppingList;
@@ -25,12 +27,15 @@ namespace ShoppingList.ViewModel
         #region Constructor
         public ViewModel()
         {
+            StartPageVisibility();
             ShoppingListList = ShoppingListSingleton.Instance.ShoppingListList;
             CreateShoppingListCommand = new RelayCommand(CreateShoppingList, null);
-            GoBackCommand = new RelayCommand(GoBackMethod, null);
-            NavigateToCreateShoppingListCommand = new RelayCommand(NavigateToCreateShoppingList, null);
-            NavigateToCreateItemCommand = new RelayCommand(NavigateToCreateItemMethod, null);
             AddItemToSelectedShoppinglistProductlistCommand = new RelayCommand(AddItemToSelectedShoppinglistProductlistMethod, null);
+
+            NavigateToCreateShoppingListCommand = new RelayCommand(ShowCreateShoppinglistPageMethod, null);
+            NavigateToCreateItemCommand = new RelayCommand(ShowCreateItemPageMethod, null);
+            NavigateBackToFrontpageCommand = new RelayCommand(StartPageVisibility, null);
+            NavigateBackToShoppingListPageCommand = new RelayCommand(ShowViewShoppinglistPageMethod, null);
         }
         #endregion
 
@@ -50,41 +55,11 @@ namespace ShoppingList.ViewModel
             set 
             { 
                 _selectedShoppingList = value;
-                ((Frame)Window.Current.Content).Navigate(typeof(ViewShoppingList));
+                ShowViewShoppinglistPageMethod();
             }
         }
         public string CategoryVM { get; set; }
         public string ShoppingListNameVM { get; set; }
-        public ICommand CreateShoppingListCommand { get; set; }
-        public ICommand GoBackCommand { get; set; }
-        public ICommand NavigateToCreateShoppingListCommand { get; set; }
-        public ICommand NavigateToCreateItemCommand { get; set; }
-        public ICommand AddItemToSelectedShoppinglistProductlistCommand { get; set; }
-
-        #region Product Properties
-        public string ItemNameVM { get; set; }
-        public string StoreVM { get; set; }
-        public int ItemAmountVM { get; set; }
-        public string ItemAmountTypeVM { get; set; }
-        public double ItemPriceVM { get; set; }
-        #endregion
-
-        #endregion
-
-        #region Methods
-        public void AddItemToSelectedShoppinglistProductlistMethod()
-        {
-            _selectedShoppingList.ProductCatalog.Add(new ProductModel(ItemNameVM, StoreVM, ItemAmountVM, ItemAmountTypeVM, ItemPriceVM));
-        }
-        public bool ShoppingListIsSelected()
-        {
-            return SelectedShoppingList != null;
-        }
-
-        //public ObservableCollection<ProductModel> SelectedProductList
-        //{
-        //    get { return _selectedShoppingList.ProductCatalog; }
-        //}
         public double SelectedListTotalPrice
         {
             get
@@ -96,15 +71,38 @@ namespace ShoppingList.ViewModel
                 return _selectedListTotalPrice;
             }
         }
-        public void NavigateToCreateItemMethod()
-        {
-            ((Frame)Window.Current.Content).Navigate(typeof(CreateItem));
+        #region Commands
+        public ICommand CreateShoppingListCommand { get; set; }
+        public ICommand AddItemToSelectedShoppinglistProductlistCommand { get; set; }
 
-        }
-        public void NavigateToCreateShoppingList()
-        {
-            ((Frame)Window.Current.Content).Navigate(typeof(CreateShoppingList));
-        }
+        public ICommand NavigateToCreateShoppingListCommand { get; set; }
+        public ICommand NavigateToCreateItemCommand { get; set; }
+        public ICommand NavigateBackToFrontpageCommand { get; set; }
+        public ICommand NavigateBackToShoppingListPageCommand { get; set; }
+        #endregion
+
+        //public ObservableCollection<ProductModel> SelectedProductList
+        //{
+        //    get { return _selectedShoppingList.ProductCatalog; }
+        //}
+
+        #region Product Properties
+        public string ItemNameVM { get; set; }
+        public string StoreVM { get; set; }
+        public int ItemAmountVM { get; set; }
+        public string ItemAmountTypeVM { get; set; }
+        public double ItemPriceVM { get; set; }
+        #endregion
+
+        #region Vibility Properties
+        public Visibility ShowFrontPageVisibility { get; set; }
+        public Visibility ShowViewShoppinglistPageVisibility { get; set; }
+        public Visibility ShowCreateShoppinglistPageVisibility { get; set; }
+        public Visibility ShowCreateItemPageVisibility { get; set; }
+        #endregion
+        #endregion
+
+        #region Methods
         public async void CreateShoppingList()
         {
             ContentDialog messageDialog = new ContentDialog()
@@ -121,16 +119,89 @@ namespace ShoppingList.ViewModel
             else
             {
                 ShoppingListSingleton.Instance.ShoppingListList.Add(new ShoppingListModel(ShoppingListNameVM, CategoryVM));
-                ((Frame)Window.Current.Content).GoBack();
+                StartPageVisibility();
                 PersistancyService.SaveShopListAsJsonAsync(ShoppingListSingleton.Instance.ShoppingListList);
 
             }
         }
-        public static void GoBackMethod()
+
+
+
+
+        public void AddItemToSelectedShoppinglistProductlistMethod()
         {
-            ((Frame)Window.Current.Content).GoBack();
+            //_selectedShoppingList.ProductCatalog.Add(new ProductModel(ItemNameVM, StoreVM, ItemAmountVM, ItemAmountTypeVM, ItemPriceVM));
+            ShoppingListSingleton.Instance.AddItemToSelectedShoppingList(_selectedShoppingList, new ProductModel(ItemNameVM, StoreVM, ItemAmountVM, ItemAmountTypeVM, ItemPriceVM));
+            PersistancyService.SaveShopListAsJsonAsync(ShoppingListSingleton.Instance.ShoppingListList);
         }
 
+
+
+
+        public bool ShoppingListIsSelected()
+        {
+            return SelectedShoppingList != null;
+        }
+
+        #region "Navigation Methods" - Warning.. NOT VERY DRY CODE
+        public void StartPageVisibility()
+        {
+            _selectedShoppingList = null;
+            ShowFrontPageVisibility = Visibility.Visible;
+            ShowViewShoppinglistPageVisibility = Visibility.Collapsed;
+            ShowCreateShoppinglistPageVisibility = Visibility.Collapsed;
+            ShowCreateItemPageVisibility = Visibility.Collapsed;
+            OnPropertyChanged(nameof(SelectedShoppingList));
+            OnPropertyChanged(nameof(ShowFrontPageVisibility));
+            OnPropertyChanged(nameof(ShowViewShoppinglistPageVisibility));
+            OnPropertyChanged(nameof(ShowCreateShoppinglistPageVisibility));
+            OnPropertyChanged(nameof(ShowCreateItemPageVisibility));
+        }
+        public void ShowViewShoppinglistPageMethod()
+        {
+            ShowFrontPageVisibility = Visibility.Collapsed;
+            ShowViewShoppinglistPageVisibility = Visibility.Visible;
+            ShowCreateShoppinglistPageVisibility = Visibility.Collapsed;
+            ShowCreateItemPageVisibility = Visibility.Collapsed;
+            OnPropertyChanged(nameof(ShowFrontPageVisibility));
+            OnPropertyChanged(nameof(ShowViewShoppinglistPageVisibility));
+            OnPropertyChanged(nameof(ShowCreateShoppinglistPageVisibility));
+            OnPropertyChanged(nameof(ShowCreateItemPageVisibility));
+        }
+        public void ShowCreateShoppinglistPageMethod()
+        {
+            ShowFrontPageVisibility = Visibility.Collapsed;
+            ShowViewShoppinglistPageVisibility = Visibility.Collapsed;
+            ShowCreateShoppinglistPageVisibility = Visibility.Visible;
+            ShowCreateItemPageVisibility = Visibility.Collapsed;
+            OnPropertyChanged(nameof(ShowFrontPageVisibility));
+            OnPropertyChanged(nameof(ShowViewShoppinglistPageVisibility));
+            OnPropertyChanged(nameof(ShowCreateShoppinglistPageVisibility));
+            OnPropertyChanged(nameof(ShowCreateItemPageVisibility));
+        }
+        public void ShowCreateItemPageMethod()
+        {
+            ShowFrontPageVisibility = Visibility.Collapsed;
+            ShowViewShoppinglistPageVisibility = Visibility.Collapsed;
+            ShowCreateShoppinglistPageVisibility = Visibility.Collapsed;
+            ShowCreateItemPageVisibility = Visibility.Visible;
+            OnPropertyChanged(nameof(ShowFrontPageVisibility));
+            OnPropertyChanged(nameof(ShowViewShoppinglistPageVisibility));
+            OnPropertyChanged(nameof(ShowCreateShoppinglistPageVisibility));
+            OnPropertyChanged(nameof(ShowCreateItemPageVisibility));
+        }
         #endregion
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion INotifyPropertyChanged
     }
 }
