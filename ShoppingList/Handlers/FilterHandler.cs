3e1545736d8dc5c44de8c10c91361e8ca5144651
Jakob-Droid <58071936+Jakob-Droid@users.noreply.github.com>
2020-02-17 11:35:27 +0100

    Filter

---
+++ ShoppingList/Handlers/FilterHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingList.Model;
+
+namespace ShoppingList.Handlers
+{
+    public class FilterHandler
+    {
+        public ObservableCollection<ProductModel> productCatalogFilters { get; set; }
+        public ViewModel.ViewModel ViewModelRef;
+
+        public FilterHandler()
+        {
+            productCatalogFilters = new ObservableCollection<ProductModel>();
+        }
+        
+
+        public ObservableCollection<ProductModel> Filter(string category)
+        {
+           var list = ViewModelRef.ProductListOnSelectedShoppingList.Where(x => x.ItemCatagory == category);
+           return new ObservableCollection<ProductModel>(list);
+        }
+        public void FilterVeggies()
+        {
+            Filter("Frugt & Grønt");
+        }
+        public void FilterMeat()
+        {
+            Filter("Kød");
+        }
+        public void FilterGeneral()
+        {
+            Filter("Generalt");
+        }
+        public void FilterToiletPaper()
+        {
+            Filter("Toiletpapir");
+        }
+        public void FilterDairy()
+        {
+            Filter("Majeriprodukter");
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
